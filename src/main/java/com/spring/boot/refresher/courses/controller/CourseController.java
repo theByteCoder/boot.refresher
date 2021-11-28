e686a9f0f266ea74be1611c3f4d47b5f6edba4a4
theByteCoder <subhasish064@gmail.com>
2021-11-28 21:27:42 +0530

    feat(app): Added h2 db dependency and custom server port

--- src/main/java/com/spring/boot/refresher/courses/controller/CourseController.java
+++ src/main/java/com/spring/boot/refresher/courses/controller/CourseController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.boot.refresher.courses.bean.Course;
@@ -15,27 +14,28 @@ public class CourseController {
 
 	@GetMapping("/courses")
 	public List<Course> getAllCourses() {
-		return Arrays.asList(new Course((long) 1, "JSP and Servelets", "theByteCoder"),
-				new Course((long) 2, "Hibernate", "theByteCoder"),
-				new Course((long) 3, "Spring framework", "theByteCoder"),
-				new Course((long) 4, "Spring MVC", "theByteCoder"), new Course((long) 5, "Spring JPA", "theByteCoder"));
+		return Arrays.asList(new Course((long) 1, "JSP and Servelets", "Subhasish"),
+				new Course((long) 2, "Hibernate", "Subh"),
+				new Course((long) 3, "Spring framework", "Piklu"),
+				new Course((long) 4, "Spring MVC", "Saheb"), new Course((long) 5, "Spring JPA", "theByteCoder"));
 	}
 
-	@RequestMapping("/courses/{id}")
+//	@RequestMapping(value = "/courses/{id}", method = RequestMethod.GET)
+	@GetMapping("/courses/{id}")
 	public Course getCourse(@PathVariable("id") String id) {
 		Course result = null;
 		switch (id) {
 		case "1":
-			result = new Course((long) 1, "JSP and Servelets", "theByteCoder");
+			result = new Course((long) 1, "JSP and Servelets", "Subhasish");
 			break;
 		case "2":
-			result = new Course((long) 2, "Hibernate", "theByteCoder");
+			result = new Course((long) 2, "Hibernate", "Subh");
 			break;
 		case "3":
-			result = new Course((long) 3, "Spring framework", "theByteCoder");
+			result = new Course((long) 3, "Spring framework", "Piklu");
 			break;
 		case "4":
-			result = new Course((long) 4, "Spring MVC", "theByteCoder");
+			result = new Course((long) 4, "Spring MVC", "Saheb");
 			break;
 		case "5":
 			result = new Course((long) 5, "Spring data JPA", "theByteCoder");
